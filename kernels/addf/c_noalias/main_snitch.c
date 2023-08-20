a454d3634116dd9c207aaa83df64840467af1a7d
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2023-08-20 17:42:16 +0100

    Measure perf counters on C 'addf' kernels

--- kernels/addf/c_noalias/main_snitch.c
+++ kernels/addf/c_noalias/main_snitch.c
@@ -3,6 +3,7 @@
 #include "diff.h"
 
 #include <snitch/runtime.h>
+#include <snitch/perf_cnt.h>
 
 #include <stddef.h>
 #include <stdint.h>
@@ -14,7 +15,17 @@ int main(void) {
     float* a = snrt_l1alloc(M * N * sizeof(float));
     if (a == NULL) return 1;
     (void)snrt_memcpy(a, A, M * N * sizeof(float));
+    uint32_t cycles_start = snrt_get_perf_counter(SNRT_PERF_CNT0);
+    uint32_t retired_start = snrt_get_perf_counter(SNRT_PERF_CNT1);
+    printf("start: %d, %d\n", cycles_start, retired_start);
+    snrt_start_perf_counter(SNRT_PERF_CNT0, SNRT_PERF_CNT_CYCLES, test_core_idx);
+    snrt_start_perf_counter(SNRT_PERF_CNT1, SNRT_PERF_CNT_RETIRED_INSTR, test_core_idx);
     addf(a, B);
+    snrt_stop_perf_counter(SNRT_PERF_CNT0);
+    snrt_stop_perf_counter(SNRT_PERF_CNT1);
+    uint32_t cycles_stop = snrt_get_perf_counter(SNRT_PERF_CNT0);
+    uint32_t retired_stop = snrt_get_perf_counter(SNRT_PERF_CNT1);
+    printf("end: %d, %d\n", cycles_stop, retired_stop);
     uint32_t ndiff = diff(a, A_PLUS_B, 0.001f /*should be proper eps*/);
     return (int)ndiff;
 }
