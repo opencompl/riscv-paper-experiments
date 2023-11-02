3266da96130ac8a27709ddd2338178a91ba82a35
Sasha Lopoukhine <superlopuh@gmail.com>
2023-11-02 20:14:38 +0000

    add flow version with split kernels

--- alexnet/run.sh
+++ alexnet/run.sh
@@ -1,4 +1,5 @@
 # Script that generated the mlir files in this folder
 
 python test.py > iree.mlir
-iree-compile --iree-input-type=torch --compile-to=input iree.mlir > linalg.mlir
+iree-compile --iree-input-type=torch --compile-to=input iree.mlir > input.mlir
+iree-compile --iree-input-type=torch --compile-to=flow --iree-util-zero-fill-elided-attrs iree.mlir > flow.mlir
