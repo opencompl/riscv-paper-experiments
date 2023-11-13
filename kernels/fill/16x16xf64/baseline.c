86f70868a46bd143c72de00ca186ded1c68315ed
Sasha Lopoukhine <superlopuh@gmail.com>
2023-11-13 10:54:36 +0000

    fill snrt wip wip wip

--- kernels/fill/16x16xf64/baseline.c
+++ kernels/fill/16x16xf64/baseline.c
@@ -5,10 +5,8 @@
 #include <stdint.h>
 
 void fill(const double x, double* y) {
-    for (uint32_t i = 0; i < M; ++i) {
-        for (uint32_t j = 0; j < N; ++j) {
-            // row-major accesses
-            y[i * N + j] = x;
-        }
+    for (uint32_t i = 0; i < M * N; ++i) {
+        // row-major accesses
+        y[i] = x;
     }
 }
