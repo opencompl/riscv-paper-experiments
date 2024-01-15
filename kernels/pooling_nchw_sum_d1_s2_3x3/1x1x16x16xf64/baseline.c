752a9b14ac7fdaf8eccd8b99f5da631c55854525
Sasha Lopoukhine <superlopuh@gmail.com>
2024-01-15 15:11:19 +0000

    pooling_nchw_sum_d1_s2_3x3

--- kernels/pooling_nchw_sum_d1_s2_3x3/1x1x16x16xf64/baseline.c
+++ kernels/pooling_nchw_sum_d1_s2_3x3/1x1x16x16xf64/baseline.c
@@ -7,7 +7,11 @@ void pooling_nchw_sum_d1_s2_3x3(const double* x, double* y) {
         for (int col = 0; col < W - 3 + 1; col += 2) {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < C; j++) {
-                    double sum = 0.0;
+                    int y_row = row / 2;
+                    int y_col = col / 2;
+                    int y_index = (i * (C * NEW_H * NEW_W)) + (j * (NEW_H * NEW_W)) +
+                                  (y_row * NEW_W) + y_col;
+                    double sum = y[y_index];
                     for (int k = 0; k < 3; k++) {
                         for (int l = 0; l < 3; l++) {
                             int x_index = (i * (C * H * W)) + (j * (H * W)) +
@@ -17,10 +21,6 @@ void pooling_nchw_sum_d1_s2_3x3(const double* x, double* y) {
                         }
                     }
                     // Store the sum in the corresponding position in y
-                    int y_row = row / 2;
-                    int y_col = col / 2;
-                    int y_index = (i * (C * NEW_H * NEW_W)) + (j * (NEW_H * NEW_W)) +
-                                  (y_row * NEW_W) + y_col;
                     y[y_index] = sum;
                 }
             }
--- kernels/pooling_nchw_sum_d1_s2_3x3/1x1x16x16xf64/snrt.c
+++ kernels/pooling_nchw_sum_d1_s2_3x3/1x1x16x16xf64/snrt.c
@@ -18,17 +18,14 @@ void pooling_nchw_sum_d1_s2_3x3(const double* x, double* y) {
 
     for (int y_row = 0; y_row < NEW_H; ++y_row) {
         for (int y_col = 0; y_col < NEW_W; ++y_col) {
-            register double sum asm("ft3") = 0.0;
-
             asm volatile(
-                "frep.o  %[nfrep], 1, 0, 0     \n\t"
-                "fadd.d %[sum], ft0, %[sum]    \n\t"
-                : [sum] "+f"(sum)
-                : [nfrep] "r"(3 * 3 - 1)
+                "fld ft3, 0(%[array])        \n\t"
+                "frep.o  %[nfrep], 1, 0, 0   \n\t"
+                "fadd.d ft3, ft0, ft3        \n\t"
+                "fsd ft3, 0(%[array])        \n\t"
+                :
+                : [array] "r"(&y[y_row * NEW_W + y_col]), [nfrep] "r"(3 * 3 - 1)
                 : "ft0", "ft1", "ft2", "memory");
-
-            // Store the maximum value in the corresponding position in y
-            y[y_row * NEW_W + y_col] = sum;
         }
     }
 
