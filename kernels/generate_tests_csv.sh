5e480bb9a5ae289c251d57d9d2c05ca1c4fcb4db
Sasha Lopoukhine <superlopuh@gmail.com>
2024-02-22 12:43:07 +0000

    wip wip wip

--- kernels/generate_tests_csv.sh
+++ kernels/generate_tests_csv.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <output_path> <input_path1> [<input_path2> ...]"
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <prefix> <output_path> <input_path1> [<input_path2> ...]"
     exit 1
 fi
 
+prefix="$1"
+shift # Shift to exclude prefix
+
 output_file="$1"
-shift  # Shift to exclude the first argument
+shift  # Shift to exclude output file
+
+# Get header from the first input file
+header=$(head -n 1 "$1")
 
 rm -f "$output_file"
+
+echo "$prefix,$header" > "$output_file"
 
 while [ "$#" -gt 0 ]; do
     file="$1"
 
     filename=$(basename "$file" .csv)
-    value=$(cat "$file")
+    value=$(tail -n +2 "$file")
     echo "$filename,$value" >> "$output_file"
 
     shift
