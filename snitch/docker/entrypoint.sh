dd9ce1bc048933b2e295d370c05eb4efca027290
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2024-03-01 15:40:34 +0000

    Update scripts

--- snitch/docker/entrypoint.sh
+++ snitch/docker/entrypoint.sh
@@ -1,37 +1,11 @@
 #!/bin/bash
 
-VALID_ARGS=$(getopt -o f,h --long force,help -- "$@")
-if [[ $? -ne 0 ]]; then
-  exit 1;
-fi
-
 VENV_DIR=venv_docker
-FORCE=0
-
-eval set -- "$VALID_ARGS"
-while [ : ]; do
-  case "$1" in
-    -f | --force)
-      FORCE=1
-      echo "Forcing venv installation"
-      shift
-      ;;
-    -h | --help)
-      echo ""
-      echo "-f | --force   Force venv installation."
-      echo "-h | --help    This help message."
-      shift
-      exit 0
-      ;;
-    --) shift;
-      break
-      ;;
-  esac
-done
+FORCE_VENV=0
 
 pushd /src/xdsl > /dev/null
 
-if [[ 1 -eq ${FORCE} ]] || [[ ! -d ${VENV_DIR} ]] || [[ ! -f ${VENV_DIR}/bin/activate ]]; then
+if [[ 1 -eq ${FORCE_VENV} ]] || [[ ! -d ${VENV_DIR} ]] || [[ ! -f ${VENV_DIR}/bin/activate ]]; then
   python3 -m venv ${VENV_DIR}
   source ${VENV_DIR}/bin/activate
 
@@ -41,6 +15,6 @@ else
   source ${VENV_DIR}/bin/activate
 fi
 
-exec "$@"
-
 popd
+
+exec "$@"
