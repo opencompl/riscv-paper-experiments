88f979f361ffea0ad888d492693a2b9c1a1ad26b
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2024-04-07 23:20:59 +0200

    Embed entrypoint in docker image (#210)
    * Embed entrypoint in docker image
    
    * Pin snitch_cluster fork commit

--- snitch/docker/entrypoint.sh
+++ snitch/docker/entrypoint.sh
@@ -1,20 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 VENV_DIR=venv_docker
 FORCE_VENV=0
 
-pushd /src/xdsl > /dev/null
-
-if [[ 1 -eq ${FORCE_VENV} ]] || [[ ! -d ${VENV_DIR} ]] || [[ ! -f ${VENV_DIR}/bin/activate ]]; then
-  python3 -m venv ${VENV_DIR}
-  source ${VENV_DIR}/bin/activate
-
-  pip install -e .
-  pip install pandas
-else
-  source ${VENV_DIR}/bin/activate
+# This entrypoint expects the source repo to be mounted at /src,
+# if this is not the case, just skip xdsl virtualenv creation.
+if [[ -d /src/xdsl ]]; then
+  pushd /src/xdsl > /dev/null
+  if [[ 1 -eq ${FORCE_VENV} ]] || [[ ! -d ${VENV_DIR} ]] || [[ ! -f ${VENV_DIR}/bin/activate ]]; then
+    python3 -m venv ${VENV_DIR}
+    source ${VENV_DIR}/bin/activate
+    pip install -e .
+    pip install pandas
+  else
+    source ${VENV_DIR}/bin/activate
+  fi
+  popd
 fi
-
-popd
 
 exec "$@"
