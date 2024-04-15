bd45e7d3b1240a9960372b9af7988c87b94f5dd6
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2024-04-15 10:04:09 +0200

    Add exploration results

---
+++ kernels/matmul/8x64x4xf64/data.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define M 8
+#define K 64
+#define N 4
+
+extern const double X[M * K];
+extern const double Y[K * N];
+extern const double G_IN[M * N];
+extern const double G_OUT[M * N];
+
