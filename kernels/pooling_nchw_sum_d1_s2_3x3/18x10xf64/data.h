c9ba801a87b37abb0fe4bd56764f9657ac185d7d
Sasha Lopoukhine <superlopuh@gmail.com>
2024-05-21 16:15:07 +0100

    parametric kernels

---
+++ kernels/pooling_nchw_sum_d1_s2_3x3/18x10xf64/data.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define N 1
+#define C 1
+#define H 18
+#define W 10
+#define NEW_H 8
+#define NEW_W 8
+
+extern const double X[N * C * H * W];
+extern const double Y_IN[N * C * NEW_H * NEW_W];
+extern const double Y_OUT[N * C * NEW_H * NEW_W];
+
+// volatile const int TEST_COUNT = 1;
+volatile const int TEST_COUNT = N * C * NEW_H * NEW_W;
+
