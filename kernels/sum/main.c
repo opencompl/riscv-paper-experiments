2f6d39b778ebc0d70b817cd635c0645c9e510df0
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2024-07-19 11:50:25 +0200

    Add f32 to dsum -> sum

--- kernels/sum/main.c
+++ kernels/sum/main.c
@@ -5,21 +5,21 @@
 #include <math.h>
 
 // Kernel provided via external definition
-void dsum(double *x, double *y, double *z);
+void sum(DTYPE *x, DTYPE *y, DTYPE *z);
 
 int main() {
     // Allocate shared local memory
     // By avoiding allocators and bumping by a known offset a base pointer
     // (snrt_l1_next()) that is the same for all the cores in the cluster, we are
     // essentially providing the same memory regions to all the cores in this cluster.
-    double *local_x = (double *)snrt_l1_next();
-    double *local_y = local_x + M * N;
-    double *local_z = local_y + M * N;
+    DTYPE *local_x = (DTYPE *)snrt_l1_next();
+    DTYPE *local_y = local_x + M * N;
+    DTYPE *local_z = local_y + M * N;
 
     // Copy data in shared local memory
     if (snrt_is_dm_core()) {
-        snrt_dma_start_1d(local_x, X, M * N * sizeof(double));
-        snrt_dma_start_1d(local_y, Y, M * N * sizeof(double));
+        snrt_dma_start_1d(local_x, X, M * N * sizeof(DTYPE));
+        snrt_dma_start_1d(local_y, Y, M * N * sizeof(DTYPE));
         snrt_dma_wait_all();
     }
 
@@ -31,14 +31,14 @@ int main() {
 
     snrt_fpu_fence();
     (void)snrt_mcycle();
-    dsum(local_x, local_y, local_z);
+    sum(local_x, local_y, local_z);
     snrt_fpu_fence();
     (void)snrt_mcycle();
 
     // Correctness check
     int nerr = 0;
     for (int i = 0; i < TEST_COUNT; i++) {
-        double d = fabs(local_z[i] - G[i]);
+        DTYPE d = fabs(local_z[i] - G[i]);
         nerr += !(d <= 1E-2);  // Make sure to take into account NaNs (e.g.: happy path
                                // on the taken branch)
     }
