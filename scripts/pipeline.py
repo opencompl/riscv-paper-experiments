5ebcbe38cec6ea6216dea4d80f76c70c80dd5d5e
Sasha Lopoukhine <superlopuh@gmail.com>
2024-07-30 15:42:53 +0200

    add rule to create pretty pipeline csv

---
+++ scripts/pipeline.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+import pandas as pd
+
+
+def merge_stats(
+    kernels_df: pd.DataFrame, regalloc_df: pd.DataFrame, output: Path | None
+):
+    kernels_df = kernels_df[kernels_df["test"] == "matmul"].set_index("impl")
+    df = regalloc_df.join(kernels_df["fpss_fpu_occupancy"])
+
+    df["fpss_fpu_occupancy"] *= 100
+
+    del df["preallocated_float"]
+    del df["preallocated_int"]
+
+    row_names = {
+        "linalg_0_xdsl": "Baseline",
+        "linalg_1_xdsl": "+ FRep",
+        "linalg_2_xdsl": "+ Streams",
+        "linalg_3_xdsl": "+ Unroll and Jam",
+        "linalg_4_xdsl": "+ Scalar Replacement",
+    }
+    col_names = {
+        "allocated_float": "F Registers",
+        "allocated_int": "X Registers",
+        "fpss_fpu_occupancy": "FPU Occupancy [%]",
+    }
+
+    df = df.rename(columns=col_names, index=row_names)
+
+    print(df.to_csv(output, float_format="%.2f"))
+
+
+def main():
+    import argparse
+
+    arg_parser = argparse.ArgumentParser()
+
+    arg_parser.add_argument("kernels", type=Path)
+    arg_parser.add_argument("regalloc_stats", type=Path)
+    arg_parser.add_argument("--output", "-o", type=Path, default=None)
+
+    args = arg_parser.parse_args()
+    kernels = args.kernels
+    regalloc_stats = args.regalloc_stats
+    output = args.output
+
+    kernels_df = pd.read_csv(kernels)
+    regalloc_df = pd.read_json(regalloc_stats, lines=True).set_index("test")
+
+    merge_stats(kernels_df, regalloc_df, output)
+
+
+if __name__ == "__main__":
+    main()
