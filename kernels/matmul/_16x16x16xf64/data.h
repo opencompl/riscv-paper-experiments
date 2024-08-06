a7b5cb7eb45f2abf01c96a0370bdc28566ec9fde
Federico Ficarelli <1379149+nazavode@users.noreply.github.com>
2024-08-06 12:21:23 +0200

    Remove archived/outdated kernels

--- kernels/matmul/_16x16x16xf64/data.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define M 16
-#define K 16
-#define N 16
-
-extern const double X[M * K];
-extern const double Y[K * N];
-extern const double G[M * N];
--- kernels/sum/_8x16xf32/data.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define M 8
-#define N 16
-
-extern const double X[N * M];
-extern const double Y[N * M];
-extern const double G[N * M];
