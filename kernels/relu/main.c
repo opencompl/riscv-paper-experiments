50415ab82fb4480185867681530073f3f017874a
Sasha Lopoukhine <superlopuh@gmail.com>
2024-08-07 09:24:04 +0100

    add relu f32 baseline

--- kernels/relu/main.c
+++ kernels/relu/main.c
@@ -5,19 +5,19 @@
 #include <math.h>
 
 // Kernel provided via external definition
-void relu(double *x, double *y);
+void relu(DTYPE *x, DTYPE *y);
 
 int main() {
     // Allocate shared local memory
     // By avoiding allocators and bumping by a known offset a base pointer
     // (snrt_l1_next()) that is the same for all the cores in the cluster, we are
     // essentially providing the same memory regions to all the cores in this cluster.
-    double *local_x = (double *)snrt_l1_next();
-    double *local_y = local_x + M * N;
+    DTYPE *local_x = (DTYPE *)snrt_l1_next();
+    DTYPE *local_y = local_x + M * N;
 
     // Copy data in shared local memory
     if (snrt_is_dm_core()) {
-        snrt_dma_start_1d(local_x, X, M * N * sizeof(double));
+        snrt_dma_start_1d(local_x, X, M * N * sizeof(DTYPE));
         snrt_dma_wait_all();
     }
 
@@ -36,7 +36,7 @@ int main() {
     // Correctness check
     int nerr = 0;
     for (int i = 0; i < TEST_COUNT; i++) {
-        double d = fabs(local_y[i] - Y[i]);
+        DTYPE d = fabs(local_y[i] - Y[i]);
         nerr += !(d <= 1E-2f);  // Make sure to take into account NaNs (e.g.: happy path
                                 // on the taken branch)
     }
