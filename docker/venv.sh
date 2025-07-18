ead00538754b545175f31a99096012429c844525
Sasha Lopoukhine <superlopuh@gmail.com>
2025-07-18 16:15:07 +0100

    wip wip wip

--- docker/venv.sh
+++ docker/venv.sh
@@ -5,15 +5,10 @@ set -euo pipefail
 VENV_DIR=/src/.venv-docker
 FORCE_VENV=0
 
-
-if [[ "${FORCE_VENV}" == "1" ]] || [[ ! -d "${VENV_DIR}" ]] || [[ ! -f "${VENV_DIR}/bin/activate" ]]; then
-  python3 -m venv "${VENV_DIR}"
-  # shellcheck disable=SC1090
-  source "${VENV_DIR}/bin/activate"
-  pip install --upgrade pip
-  pip install -r /src/requirements.txt
+if [[ ! -d ${VENV_DIR} ]]; then
+  uv venv ${VENV_DIR}
+  source ${VENV_DIR}/bin/activate
+  uv pip install -r /src/requirements.txt
 else
-  # shellcheck disable=SC1090
-  source "${VENV_DIR}/bin/activate"
+  source ${VENV_DIR}/bin/activate
 fi
-
