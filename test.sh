5f79717a2e5b5c4ca95b3603fecf65dac64db8a9
Mia Sophie Zerdick <szerdick@student.ethz.ch>
2026-03-23 19:37:19 +0000

    own notes

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 !#/bin/bash
 
+
+#### f64 version ###
+
 # Generate an mlir file for a single test from the mlir template
 snakemake --cores 1 kernels/exp_micro/16xf64/linalg.mlir
 
@@ -11,3 +14,30 @@ xdsl-opt kernels/exp_micro/16xf64/linalg.mlir  -p expand-math-to-polynomials
 
 # Run linalg version measurement on the generated mlir file
 snakemake --cores 1 --forcerun kernels/exp_micro/16xf64/linalg_xdsl.profile.json
+
+
+### f32 version ###
+
+snakemake --cores 1 kernels/exp_micro/16xf32/linalg.mlir
+
+xdsl-opt kernels/exp_micro/16xf32/linalg.mlir
+
+xdsl-opt kernels/exp_micro/16xf32/linalg.mlir  -p expand-math-to-polynomials
+
+snakemake --cores 1 --forcerun kernels/exp_micro/16xf32/linalg_xdsl.profile.json
+
+
+
+### Step by step execution for f32 ###
+
+# Generate params file
+snakemake --cores 1 kernels/exp_micro/16xf32/params.json
+
+# generate xdsl-ready mlir file
+snakemake --cores 1 kernels/exp_micro/16xf32/linalg_xdsl.xdsl.mlir
+
+# assembly
+snakemake --cores 1 kernels/exp_micro/16xf32/linalg_xdsl.S
+
+# object file
+
